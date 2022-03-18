0f6e9beaf4a955b39539d10a59e3b7ba6d1eea36
VICTOR FERREIRA DO VALE <vferreiradovale@gmail.com>
2022-03-17 23:01:04 -0300

    Cadastro de bandas finalizado

---
+++ src/data/bandRegisterData.ts
@@ -0,0 +1,6 @@
+import { connection } from "./dataBase/connection"
+
+
+export const bandRegisterData = async( id: string, name: string, music_genre: string, responsible: string) => {
+    await connection().insert({ id, name, music_genre, responsible}).into('NOME_TABELA_BANDAS')
+}
